ca0f2639be4a1e52a9a7430fe1ce420f07a75642
Jhonatan Seminario <seminariodev@gmail.com>
2024-05-20 01:49:44 -0500

    correcion voltear cartas

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     
     cardsContainer.innerHTML = "";
     [...cards].sort(() => Math.random() - 0.5).forEach(card => cardsContainer.appendChild(card));
+
+    cards.forEach(card => {
+        card.addEventListener("click", function() {
+            card.style.transform = "perspective(256px) rotateY(180deg)";
+            card.querySelector(".front-card").style.transform = "perspective(256px) rotateY(180deg)";
+            card.querySelector(".back-card").style.transform = "perspective(256px) rotateY(360deg)";
+        });
+    });
 });
