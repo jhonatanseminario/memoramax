040e485ce9b1ec027e9e9a3637de8023955e8c20
Jhonatan Seminario <seminariodev@gmail.com>
2024-05-20 01:20:49 -0500

    optimizacion codigo

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     let cardsContainer = document.querySelector(".cards-container");
     let cards = document.querySelectorAll(".card");
-    let cardsArray = Array.from(cards);
-
-    function order() {
-        return Math.random() - 0.5;
-    }
-    cardsArray.sort(order);
-
+    
     cardsContainer.innerHTML = "";
-    cardsArray.forEach(function(card) {
-        cardsContainer.appendChild(card);
-    });
+    [...cards].sort(() => Math.random() - 0.5).forEach(card => cardsContainer.appendChild(card));
 });
