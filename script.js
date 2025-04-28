2a5961b4609a4515a413ef4a3cbe8e91b0e5902e
Jhonatan Seminario <seminariodev@gmail.com>
2025-04-28 01:42:12 -0500

    reorganizar el favicon y actualizar la estructura de cartas en el html; optimizar el estilo css y mejorar la lógica javascript del juego de cartas

--- script.js
+++ script.js
@@ -1,57 +1,93 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let cardsContainer = document.querySelector(".cards-container");
-    let cards = document.querySelectorAll(".card");
-    let flippedCards = 0;
-    let flippedCardNumbers = [];
-    let flippedCardElements = [];
+//*==========================================================================*//
+//*                      INICIALIZAR VARIABLES GLOBALES                      *//
+//*==========================================================================*//
+
+const cardsContainer = document.querySelector(".cards-container");
+const cards = document.querySelectorAll(".card");
+
+let flippedCardCount = 0;
+let flippedCardsValues = [];
+let flippedCardNodes = [];
+
+
+//*==========================================================================*//
+//*                     INSERTAR CARTAS EN EL CONTENEDOR                     *//
+//*==========================================================================*//
+
+const values = ["😠", "🙁", "😦", "😂", "😭", "😀", "😍", "😎"];
+const cardValues = [...values, ...values];
+
+cardValues.sort(() => Math.random() - 0.5);
+
+cardValues.forEach( cardValue => {
+    const card = document.createElement("div");
+    card.classList.add("card");
     
-    cardsContainer.innerHTML = "";
-    [...cards].sort(() => Math.random() - 0.5).forEach(card => cardsContainer.appendChild(card));
+    const frontCard = document.createElement("div");
+    frontCard.classList.add("front-card");
+    frontCard.textContent = "?";
+    
+    const backCard = document.createElement("div");
+    backCard.classList.add("back-card");
+    backCard.textContent = cardValue;
+    
+    card.appendChild(frontCard);
+    card.appendChild(backCard);
+    cardsContainer.appendChild(card);
 
-    function flipCard(card) {
-        card.classList.add("flipped");
-        card.style.transform = "perspective(256px) rotateY(180deg)";
-        card.querySelector(".front-card").style.transform = "perspective(256px) rotateY(180deg)";
-        card.querySelector(".back-card").style.transform = "perspective(256px) rotateY(0deg)";
-    }
+    card.addEventListener( "click", () => {
+        if (card.classList.contains('flipped') || flippedCardCount === 2) return;
 
-    function unflipCard(card) {
-        card.classList.remove("flipped");
-        card.style.transform = "perspective(256px) rotateY(0deg)";
-        card.querySelector(".front-card").style.transform = "perspective(256px) rotateY(0deg)";
-        card.querySelector(".back-card").style.transform = "perspective(256px) rotateY(180deg)";
-    }
+        flipCard(card);
 
-    function checkMatch() {
-        if (flippedCardNumbers.length === 2) {
-            const [number1, number2] = flippedCardNumbers;
-            if (number1 === number2) {
-                setTimeout(() => {
-                    flippedCardElements.forEach(card => card.classList.add('matched'));
-                    flippedCardNumbers = [];
-                    flippedCardElements = [];
-                    flippedCards = 0;
-                }, 500);
-            } else {
-                setTimeout(() => {
-                    flippedCardElements.forEach(card => unflipCard(card));
-                    flippedCardNumbers = [];
-                    flippedCardElements = [];
-                    flippedCards = 0;
-                }, 1000);  
-            }
-        }
-    }
+        flippedCardCount++;
+        flippedCardsValues.push(card.querySelector(".back-card").textContent);
+        flippedCardNodes.push(card);
 
-    cards.forEach(card => {
-        card.addEventListener("click", function() {
-            if (!card.classList.contains('flipped') && flippedCards < 2) {
-                flipCard(card);
-                flippedCards++;
-                flippedCardNumbers.push(card.querySelector(".back-card").textContent);
-                flippedCardElements.push(card);
-                checkMatch();
-            }
-        });
+        checkMatch();
     });
 });
+
+
+//*==========================================================================*//
+//*                           FUNCIONES AUXILIARES                           *//
+//*==========================================================================*//
+
+function checkMatch() {
+    if (flippedCardsValues.length !== 2) return;
+
+    const [value1, value2] = flippedCardsValues;
+    
+    if (value1 === value2) {
+        setTimeout(() => {
+            flippedCardCount = 0;
+            flippedCardsValues = [];
+            flippedCardNodes = [];
+        }, 500); 
+    }
+
+    else {
+        setTimeout(() => {
+            flippedCardNodes.forEach( card => unflipCard(card));
+            flippedCardCount = 0;
+            flippedCardsValues = [];
+            flippedCardNodes = [];
+        }, 500);  
+    }
+}
+
+function flipCard(card) {
+    card.classList.add("flipped");
+    
+    card.style.transform = "perspective(256px) rotateY(180deg)";
+    card.querySelector(".front-card").style.transform = "perspective(256px) rotateY(180deg)";
+    card.querySelector(".back-card").style.transform = "perspective(256px) rotateY(0deg)";
+}
+
+function unflipCard(card) {
+    card.classList.remove("flipped");
+    
+    card.style.transform = "perspective(256px) rotateY(0deg)";
+    card.querySelector(".front-card").style.transform = "perspective(256px) rotateY(0deg)";
+    card.querySelector(".back-card").style.transform = "perspective(256px) rotateY(180deg)";
+}
